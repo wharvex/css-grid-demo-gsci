ab33f6dd276570a69caeebfe65ad5e0ec2eb1b4b
Tim <81175852+wharvex@users.noreply.github.com>
2026-02-09 20:39:23 -0500

    updates

---
+++ script.js
@@ -0,0 +1,20 @@
+function addClass(element, add) {
+    if (element.className === "") {
+        element.className = add;
+    } else {
+        element.className += " " + add;
+    }
+}
+
+// Adapted from https://stackoverflow.com/a/6976865
+function removeClass(element, remove) {
+    var newClassName = "";
+    var i;
+    var classes = element.className.split(" ");
+    for (i = 0; i < classes.length; i++) {
+        if (classes[i] !== remove) {
+            newClassName += classes[i] + " ";
+        }
+    }
+    element.className = newClassName;
+}
